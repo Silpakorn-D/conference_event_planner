b036b2b8973482654754b4bfcbdcd4ec8f89eb86
Silpakorn Dachanuwattana <art.silpakorn@gmail.com>
2024-10-26 06:28:38 -0400

    Write code for add-ons

--- src/avSlice.js
+++ src/avSlice.js
@@ -2,17 +2,54 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const avSlice = createSlice({
   name: "av",
-  initialState: [
+  initialState: [//Initialize the initialState array variable with objects to provide a data structure
+        {
+        img: "https://pixabay.com/images/download/business-20031_640.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+        },
+        {
+            img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+            name: "Speaker",
+            cost: 35,
+            quantity: 0,
+        },
+        {
+            img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+            name: "Microphones",
+            cost: 45,
+            quantity: 0,
+        },
+        {
+            img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+            name: "Whiteboards",
+            cost: 80,
+            quantity: 0,
+        },
+        {
+            img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+            name: "Signage",
+            cost: 80,
+            quantity: 0,
+        },
+
     
   ],
 
 
   reducers: {
-    incrementAvQuantity: (state, action) => {
-      
+    incrementAvQuantity: (state, action) => {//reducer function increments the quantity of a specific item in the state.
+        const item = state[action.payload];//The action.payload object contains the identifier of the item to increment
+        if (item) {
+            item.quantity++;
+        }         
     },
-    decrementAvQuantity: (state, action) => {
-     
+    decrementAvQuantity: (state, action) => {//reducer function decrements the quantity of a specific item in the state.
+        const item = state[action.payload];//The action.payload object contains the item identifier to decrement
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }     
     },
   },
 });
--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './avSlice'; //Provide the avSlice
 export default configureStore({//Create the Redux store with the configureStore() function from @reduxjs/toolkit
   reducer: {
     venue: venueReducer,// a reducer called venue(), imported from venueSlice.js
+    av: avReducer,//a reducer called av(), imported from avSlice.js
   },
 });
 //This code creates a global Redux store using the @reduxjs/toolkit\ configureStore() function 
